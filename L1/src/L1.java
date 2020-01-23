f349622b76d556868ec6f6aa4b87ace596f87394
Blake Brisben <blakebri@sardine.cs.colostate.edu>
2020-01-23 09:44:05 -0700

    L1 Complete

---
+++ L1/src/L1.java
@@ -0,0 +1,16 @@
+// L1 Assignment
+// Author: Blake Brisben
+// Date:   1/23/2020
+// Class:  CS165
+// Email:  blakebri@rams.colostate.edu
+
+
+public class L1 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java programming is great!");
+	}
+
+}
